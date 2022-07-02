0c8f3fe8fe8d02dfa1f0dce646b8f124e12cb671
ToyB-Chan <jasjason0otto@gmail.com>
2022-07-02 19:01:05 +0200

    Added rotating box reflection capture
    Normal reflection captures don't get rotated when loading them in via load level instance, this actor will handle such rotations (only per 90°!)

---
+++ Source/scppu/Private/RotatingReflectionCapture/RotatingBoxReflectionCapture.cpp
@@ -0,0 +1,199 @@
+
+
+
+#include "Components/BoxComponent.h"
+#include "Components/BillboardComponent.h"
+#include "Components/BoxReflectionCaptureComponent.h"
+#include "Engine/ReflectionCapture.h"
+#include "Components/SceneCaptureComponentCube.h"
+#include "Engine/TextureRenderTargetCube.h"
+#include "Engine/TextureCube.h"
+#include "LevelEditor.h"
+#include "Editor.h"
+#include "RotatingReflectionCaptures/RotatingBoxReflectionCapture.h"
+
+DEFINE_LOG_CATEGORY(LogRotatingRefCap);
+
+// Sets default values
+ARotatingBoxReflectionCapture::ARotatingBoxReflectionCapture()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	this->PrimaryActorTick.bCanEverTick = false;
+	this->bRunConstructionScriptOnDrag = false;
+	FAttachmentTransformRules AttachmentRules = FAttachmentTransformRules(EAttachmentRule::KeepRelative, EAttachmentRule::KeepRelative, EAttachmentRule::KeepRelative, false);
+
+	// Root component
+	this->Root = CreateDefaultSubobject<USceneComponent >("Root", false);
+	this->Root->SetMobility(EComponentMobility::Static);
+	this->Root->SetWorldScale3D(FVector(500, 500, 200));
+	this->SetRootComponent(this->Root);
+
+#if WITH_EDITOR
+	// Sprite component
+	this->Sprite = CreateDefaultSubobject<UBillboardComponent>("Sprite", false);
+	this->Sprite->SetSprite(ConstructorHelpers::FObjectFinder<UTexture2D>(TEXT("Texture2D'/Engine/EditorResources/S_ReflActorIcon.S_ReflActorIcon'")).Object);
+	this->Sprite->bIsScreenSizeScaled = true;
+	this->Sprite->ScreenSize = 0.0025f;
+	this->Sprite->SetRelativeLocation(FVector(0, 0, 0));
+	this->Sprite->AttachToComponent(this->Root, AttachmentRules);
+	
+	// Box component
+	this->Box = CreateDefaultSubobject<UBoxComponent >("Box", false);
+	this->Box->SetBoxExtent(FVector(1, 1, 1), false);
+	this->Box->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	this->Box->SetRelativeLocation(FVector(0, 0, 0));
+	this->Box->AttachToComponent(this->Root, AttachmentRules);
+#endif
+
+	// Reflection capture rotation 0
+	this->RefCap0 = CreateDefaultSubobject<UBoxReflectionCaptureComponent>("BoxReflectionCaptureRot0", false);
+	this->RefCap0->ReflectionSourceType = EReflectionSourceType::SpecifiedCubemap;
+	this->RefCap0->SourceCubemapAngle = 0;
+	this->RefCap0->SetRelativeLocation(FVector(0, 0, 0));
+	this->RefCap0->AttachToComponent(this->Root, AttachmentRules);
+	this->RefCap0->SetVisibility(true);
+
+	// Reflection capture rotation 90
+	this->RefCap90 = CreateDefaultSubobject<UBoxReflectionCaptureComponent>("BoxReflectionCaptureRot90", false);
+	this->RefCap90->ReflectionSourceType = EReflectionSourceType::SpecifiedCubemap;
+	this->RefCap90->SourceCubemapAngle = 90;
+	this->RefCap90->SetRelativeLocation(FVector(0, 0, 0));
+	this->RefCap90->AttachToComponent(this->Root, AttachmentRules);
+	this->RefCap90->SetVisibility(true);
+
+	// Reflection capture rotation 180
+	this->RefCap180 = CreateDefaultSubobject<UBoxReflectionCaptureComponent>("BoxReflectionCaptureRot180", false);
+	this->RefCap180->ReflectionSourceType = EReflectionSourceType::SpecifiedCubemap;
+	this->RefCap180->SourceCubemapAngle = 180;
+	this->RefCap180->SetRelativeLocation(FVector(0, 0, 0));
+	this->RefCap180->AttachToComponent(this->Root, AttachmentRules);
+	this->RefCap180->SetVisibility(true);
+
+	// Reflection capture rotation 270
+	this->RefCap270 = CreateDefaultSubobject<UBoxReflectionCaptureComponent>("BoxReflectionCaptureRot270", false);
+	this->RefCap270->ReflectionSourceType = EReflectionSourceType::SpecifiedCubemap;
+	this->RefCap270->SourceCubemapAngle = 270;
+	this->RefCap270->SetRelativeLocation(FVector(0, 0, 0));
+	this->RefCap270->AttachToComponent(this->Root, AttachmentRules);
+	this->RefCap270->SetVisibility(true);
+
+	// Scene capture cube
+#if WITH_EDITOR
+	this->SceneCaptureCube = CreateDefaultSubobject<USceneCaptureComponentCube>("SceneCaptureCube", false);
+	this->SceneCaptureCube->bCaptureEveryFrame = false;
+	this->SceneCaptureCube->bCaptureOnMovement = false;
+	this->SceneCaptureCube->ShowFlags.ReflectionEnvironment = false;
+	this->SceneCaptureCube->SetRelativeLocation(FVector(0, 0, 0));
+	this->SceneCaptureCube->SetRelativeScale3D(FVector(0.00001, 0.00001, 0.00001));
+	this->SceneCaptureCube->AttachToComponent(this->Root, AttachmentRules);
+#endif
+}
+
+void ARotatingBoxReflectionCapture::UpdateCapture()
+{
+#if WITH_EDITOR
+	// Only allow updates every two seconds (to avoid performance problems)
+	if ((FDateTime::UtcNow() - this->LastUpdate).GetTotalSeconds() < 2.0f)
+	{
+		UE_LOG(LogRotatingRefCap, Warning, TEXT("%s: Not updating, last update was at least 2s ago"), *this->GetName());
+		return;
+	}
+
+	this->LastUpdate = FDateTime::UtcNow();
+
+	// Dispose old render target and static texture
+	if (IsValid(this->RenderTarget))
+	{
+		this->RenderTarget->ConditionalBeginDestroy();
+	}
+
+	if (IsValid(this->StaticTexture))
+	{
+		this->StaticTexture->ConditionalBeginDestroy();
+	}
+
+	// Create new render target
+	this->RenderTarget = NewObject<UTextureRenderTargetCube>(this);
+	this->RenderTarget->ClearColor = FLinearColor::Green;
+	this->RenderTarget->Init(256, EPixelFormat::PF_FloatRGBA);
+	this->RenderTarget->UpdateResourceImmediate(true);
+	UE_LOG(LogRotatingRefCap, Log, TEXT("%s: Created new render target"), *this->GetName());
+
+	// Capture scence
+	this->SceneCaptureCube->TextureTarget = this->RenderTarget;
+	this->SceneCaptureCube->CaptureScene();
+	UE_LOG(LogRotatingRefCap, Log, TEXT("%s: Captured scene"), *this->GetName());
+
+	// Create new static texture
+	this->StaticTexture = NewObject<UTextureCube>(this, FName());
+
+	// Draw render target to static texture
+	this->StaticTexture = this->RenderTarget->ConstructTextureCube(this->StaticTexture->GetOuter(), this->StaticTexture->GetName(), this->RenderTarget->GetMaskedFlags());
+	this->StaticTexture->Modify();
+	this->StaticTexture->MarkPackageDirty();
+	this->StaticTexture->PostEditChange();
+	this->StaticTexture->UpdateResource();
+	UE_LOG(LogRotatingRefCap, Log, TEXT("%s: Created new static texture"), *this->GetName());
+
+	// Apply new static texture to reflection capture
+	this->RefCap0->Cubemap = this->StaticTexture;
+	this->RefCap90->Cubemap = this->StaticTexture;
+	this->RefCap180->Cubemap = this->StaticTexture;
+	this->RefCap270->Cubemap = this->StaticTexture;
+	UE_LOG(LogRotatingRefCap, Log, TEXT("%s: Updated reflection captures"), *this->GetName());
+#endif
+}
+
+void ARotatingBoxReflectionCapture::OnConstruction(const FTransform& Transform)
+{
+	if (this->GetWorld()->WorldType == EWorldType::Editor || this->GetWorld()->WorldType == EWorldType::EditorPreview)
+	{
+		this->UpdateCapture();
+		this->PlacedRotation = this->GetActorRotation();
+	}
+}
+
+// Called when the game starts or when spawned
+void ARotatingBoxReflectionCapture::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Delete reflection captures based on our "new" rotation
+    int Rotation = FMath::DivideAndRoundNearest((this->GetActorRotation() - this->PlacedRotation).GetDenormalized().Yaw, 90.0f);
+	UE_LOG(LogRotatingRefCap, Log, TEXT("%s: Detected rotation %d"), *this->GetName(), Rotation);
+	switch (Rotation)
+	{
+	case 0:
+		this->RefCap0->SetVisibility(true);
+		this->RefCap90->DestroyComponent();
+		this->RefCap180->DestroyComponent();
+		this->RefCap270->DestroyComponent();
+		break;
+	case 1:
+		this->RefCap270->SetVisibility(true); //Soruce angle rotates in the opposite direction
+		this->RefCap0->DestroyComponent();
+		this->RefCap90->DestroyComponent();
+		this->RefCap180->DestroyComponent();
+		break;
+	case 2:
+		this->RefCap180->SetVisibility(true);
+		this->RefCap270->DestroyComponent();
+		this->RefCap0->DestroyComponent();
+		this->RefCap90->DestroyComponent();
+		break;
+	case 3:
+		this->RefCap90->SetVisibility(true); //Soruce angle rotates in the opposite direction
+		this->RefCap180->DestroyComponent();
+		this->RefCap270->DestroyComponent();
+		this->RefCap0->DestroyComponent();
+		break;
+
+	}
+}
+
+// Called every frame
+void ARotatingBoxReflectionCapture::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/scppu/Public/RotatingReflectionCaptures/RotatingBoxReflectionCapture.h
@@ -0,0 +1,81 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RotatingBoxReflectionCapture.generated.h"
+
+class UBoxComponent;
+class UBoxReflectionCaptureComponent;
+class USceneCaptureComponentCube;
+class UTextureRenderTargetCube;
+
+DECLARE_LOG_CATEGORY_EXTERN(LogRotatingRefCap, Log, All);
+
+UCLASS(hidecategories = (Collision, Attachment, Actor))
+class SCPPU_API ARotatingBoxReflectionCapture : public AActor
+{
+	GENERATED_BODY()
+	
+	//// Properties ////
+public:
+	UPROPERTY()
+		USceneComponent* Root;
+
+#if WITH_EDITOR
+	UPROPERTY()
+		UBillboardComponent* Sprite;
+
+	UPROPERTY()
+		UBoxComponent* Box;
+#endif
+
+	UPROPERTY()
+		UBoxReflectionCaptureComponent* RefCap0;
+
+	UPROPERTY()
+		UBoxReflectionCaptureComponent* RefCap90;
+
+	UPROPERTY()
+		UBoxReflectionCaptureComponent* RefCap180;
+
+	UPROPERTY()
+		UBoxReflectionCaptureComponent* RefCap270;
+
+#if WITH_EDITOR
+	UPROPERTY()
+		USceneCaptureComponentCube* SceneCaptureCube;
+#endif
+
+	UPROPERTY()
+		FRotator PlacedRotation;
+
+#if WITH_EDITOR
+	UPROPERTY()
+		FDateTime LastUpdate;
+#endif
+
+	UPROPERTY(VisibleAnywhere, Category = "Custom Reflection Capture")
+		UTextureRenderTargetCube* RenderTarget = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Custom Reflection Capture")
+		UTextureCube* StaticTexture = nullptr;
+
+	//// Functions ////	
+public:
+	// Sets default values for this actor's properties
+	ARotatingBoxReflectionCapture();
+
+	UFUNCTION(CallInEditor, Category = "Custom Reflection Capture")
+		void UpdateCapture();
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
