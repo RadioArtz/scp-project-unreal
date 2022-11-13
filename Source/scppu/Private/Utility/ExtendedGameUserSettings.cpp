abe8d0b197ad99db7e859cce998b41c1f3852dec
ToyB-Chan <jasjason0otto@gmail.com>
2022-11-13 20:09:41 +0100

    Extended game user settings so we can add our own settings to it

---
+++ Source/scppu/Private/Utility/ExtendedGameUserSettings.cpp
@@ -0,0 +1,116 @@
+
+
+
+#include "Utility/ExtendedGameUserSettings.h"
+
+UExtendedGameUserSettings* UExtendedGameUserSettings::GetExtendedGameUserSettings()
+{
+	return Cast<UExtendedGameUserSettings>(UGameUserSettings::GetGameUserSettings());
+}
+
+void UExtendedGameUserSettings::SetFSR1Enabled(bool bEnable)
+{
+	this->bUseFSR1 = bEnable;
+}
+
+bool UExtendedGameUserSettings::IsFSR1Enabled() const
+{
+	return this->bUseFSR1;
+}
+
+void UExtendedGameUserSettings::SetFSR2Quality(int Value)
+{
+	this->FSR2QualityLevel = FMath::Clamp(Value, 0, 4);
+}
+
+int UExtendedGameUserSettings::GetFSR2Quality() const
+{
+	return this->FSR2QualityLevel;
+}
+
+void UExtendedGameUserSettings::SetScreenGamma(float Value)
+{
+	this->ScreenGammaLevel = Value;
+}
+
+float UExtendedGameUserSettings::GetScreenGamma() const
+{
+	return this->ScreenGammaLevel;
+}
+
+void UExtendedGameUserSettings::ApplyNonResolutionSettings()
+{
+	Super::ApplyNonResolutionSettings();
+
+	// Update FSR1 CVar
+	{
+		FString ConfigSection = TEXT("SystemSettings");
+#if WITH_EDITOR
+		if (GIsEditor)
+		{
+			ConfigSection = TEXT("SystemSettingsEditor");
+		}
+#endif
+		int FSR1Value = 0;
+		if (GConfig->GetInt(*ConfigSection, TEXT("r.FidelityFX.FSR.Enabled"), FSR1Value, GEngineIni))
+		{
+			// FSR1 was already set by system settings. We are capable of setting it here.
+		}
+		else
+		{
+			static auto CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("r.FidelityFX.FSR.Enabled"));
+			CVar->Set(this->IsFSR1Enabled(), ECVF_SetByGameSetting);
+		}
+	}
+
+	// Update FSR2 CVar
+	{
+		FString ConfigSection = TEXT("SystemSettings");
+#if WITH_EDITOR
+		if (GIsEditor)
+		{
+			ConfigSection = TEXT("SystemSettingsEditor");
+		}
+#endif
+		int FSR2EnabledValue = 0;
+		int FSR2QualityValue = 0;
+		if (GConfig->GetInt(*ConfigSection, TEXT("r.FidelityFX.FSR2.Enabled"), FSR2EnabledValue, GEngineIni) || GConfig->GetInt(*ConfigSection, TEXT("r.FidelityFX.FSR2.QualityMode"), FSR2QualityValue, GEngineIni))
+		{
+			// FSR2 was already set by system settings. We are capable of setting it here.
+		}
+		else
+		{
+			{
+				static auto CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("r.FidelityFX.FSR2.Enabled"));
+				CVar->Set(this->FSR2QualityLevel != 0, ECVF_SetByGameSetting);
+			}
+			{
+				static auto CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("r.FidelityFX.FSR2.QualityMode"));
+				CVar->Set(this->FSR2QualityLevel, ECVF_SetByGameSetting);
+			}
+		}
+	}
+
+	// Update Gamma CVar (UPDATE: gamma is not a cvar (?) still have to figure out what it is.)
+	/**
+	{
+		FString ConfigSection = TEXT("SystemSettings");
+#if WITH_EDITOR
+		if (GIsEditor)
+		{
+			ConfigSection = TEXT("SystemSettingsEditor");
+		}
+#endif
+		float GammaValue = 0;
+		if (GConfig->GetFloat(*ConfigSection, TEXT("gamma"), GammaValue, GEngineIni))
+		{
+			// FSR2 was already set by system settings. We are capable of setting it here.
+		}
+		else
+		{
+			static auto CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("gamma"));
+			CVar->Set(this->ScreenGammaLevel != 0, ECVF_SetByGameSetting);
+		}
+	}
+	*/
+}
---
+++ Source/scppu/Public/Utility/ExtendedGameUserSettings.h
@@ -0,0 +1,66 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "ExtendedGameUserSettings.generated.h"
+
+UCLASS()
+class SCPPU_API UExtendedGameUserSettings : public UGameUserSettings
+{
+	GENERATED_BODY()
+	
+	//// Properties ////
+protected:
+	// If true, FSR 1 will be used
+	UPROPERTY(config)
+		bool bUseFSR1 = false;
+
+	/**
+	 * Quality level for FSR 2
+	 *	0 = Disabled
+	 *	1 = Quality
+	 *	...
+	 *	4 = Ultra Performance
+	 */
+	UPROPERTY(config)
+		int FSR2QualityLevel = 0;
+
+	// Gamma level to use. Higher value = more brightness
+	UPROPERTY(config)
+		float ScreenGammaLevel = 2.2f;
+
+	//// Functions ////	
+public:
+	// Returns the game local machine settings (resolution, windowing mode, scalability settings, etc...), but it's extended and has more settings
+	UFUNCTION(BlueprintCallable, Category = Settings)
+		static UExtendedGameUserSettings* GetExtendedGameUserSettings();
+
+	// Sets the user setting for FSR1
+	UFUNCTION(BlueprintCallable, Category = Settings)
+		void SetFSR1Enabled(bool bEnable);
+
+	// Returns the user setting for FSR1
+	UFUNCTION(BlueprintCallable, Category = Settings)
+		bool IsFSR1Enabled() const;
+
+	// Sets the user setting for FSR2 (0..4, higher is stronger)
+	// @param Value 0:disabled, 1:quality, 2:balanced, 3:performance, 4:ultra performance (gets clamped if needed)
+	UFUNCTION(BlueprintCallable, Category = Settings)
+		void SetFSR2Quality(int Value);
+
+	// Returns the user setting for FSR2 (0..4, higher is stronger)
+	UFUNCTION(BlueprintCallable, Category = Settings)
+		int GetFSR2Quality() const;
+
+	// Sets the user setting for screen gamma
+	UFUNCTION(BlueprintCallable, Category = Settings)
+		void SetScreenGamma(float Value);
+
+	// Returns the user setting for screen gamma
+	UFUNCTION(BlueprintCallable, Category = Settings)
+		float GetScreenGamma() const;
+
+	virtual void ApplyNonResolutionSettings() override;
+};
